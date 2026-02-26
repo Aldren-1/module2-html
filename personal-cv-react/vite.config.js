9452adca469f2f40304af6ee4167196fb5453fd6
Aldren-1 <restauroaldrin@gmail.com>
2026-02-26 23:18:23 +0800

    added

--- personal-cv-react/vite.config.js
+++ personal-cv-react/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/module2-html/', 
+  base: '/module2-html/',
   plugins: [react()],
 })
